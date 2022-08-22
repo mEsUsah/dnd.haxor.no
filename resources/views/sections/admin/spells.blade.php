2fe1b8cda448a9938c3768f6a9ccb608065394ae
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-08-22 18:16:50 +0000

    implement variables for spell levels

--- resources/views/sections/admin/spells.blade.php
+++ resources/views/sections/admin/spells.blade.php
@@ -49,16 +49,9 @@
                             <div class="col-sm-9">
                                 <select class="form-select" aria-label="Default select example" name="level">
                                     <option selected>Select...</option>
-                                        <option value="0">0 - Cantrip</option>
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
-                                        <option value="6">6</option>
-                                        <option value="7">7</option>
-                                        <option value="8">8</option>
-                                        <option value="9">9</option>
+                                    @foreach (config('variables.spellLevel') as $id => $level)
+                                        <option value="{{ $id }}">{{ $level }}</option>
+                                    @endforeach
                                 </select>
                             </div>
                         </div>
