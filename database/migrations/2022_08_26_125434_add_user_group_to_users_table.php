069e034304ca3357f56f846aa7c8ac8f76d80651
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-08-26 13:01:59 +0000

    add user group table, and implement into user table

---
+++ database/migrations/2022_08_26_125434_add_user_group_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('user_group_id')->constrained()->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['user_group_id']);
+            $table->dropIndex(['user_group_id']);
+        });
+    }
+};
