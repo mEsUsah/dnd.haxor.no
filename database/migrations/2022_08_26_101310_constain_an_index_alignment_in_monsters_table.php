2e40e5301ca93f146521ded0df9f80481d24873d
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-08-26 10:19:01 +0000

    rename, index and constarin alignment in monsters table

---
+++ database/migrations/2022_08_26_101310_constain_an_index_alignment_in_monsters_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('monsters', function (Blueprint $table) {
+            $table->integer('alignment_id')->constrained()->index()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('monsters', function (Blueprint $table) {
+            $table->dropForeign(['alignment_id']);
+            $table->dropIndex(['alignment_id']);
+        });
+    }
+};
