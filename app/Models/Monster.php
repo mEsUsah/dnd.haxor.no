901b1b03e5babbd0e20ed8d5a56813d8b6069e4a
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-08-26 10:22:41 +0000

    relate monsters model to alignment model

--- app/Models/Monster.php
+++ app/Models/Monster.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Alignment;
 use App\Models\Challenge;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -36,5 +37,10 @@ class Monster extends Model
     public function challenge()
     {
         return $this->belongsTo(Challenge::class);
+    }
+
+    public function alignment()
+    {
+        return $this->belongsTo(Alignment::class);
     }
 }
