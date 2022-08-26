2c946bd998cb8d2371d82d32071780aa487a4e6d
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-08-26 07:09:53 +0000

    implement <old> functionality on form

--- resources/views/sections/admin/monsters/form.blade.php
+++ resources/views/sections/admin/monsters/form.blade.php
@@ -28,10 +28,7 @@
                             <label for="name" class="col-sm-3 col-form-label">Name</label>
                             <div class="col-sm-9">
                                 <input type="text" class="form-control" id="name" name="name"
-                                @if ($monster)
-                                    value="{{ $monster->name }}"
-                                @endif
-                                >
+                                    value="{{ old('name', optional($monster)->name) }}">
                             </div>
                         </div>
 
@@ -41,11 +38,10 @@
                                 <select class="form-select" aria-label="Default select example" name="type">
                                     <option selected>Select...</option>
                                     @foreach (config('variables.cratureType') as $id => $type)
-                                        <option value="{{ $id }}" 
-                                        @if ($monster)
-                                                {{ $monster->type == $id ? 'selected' : '' }}
-                                        @endif
-                                            >{{ $type }}</option>
+                                    <option value="{{ $id }}" 
+                                        {{ old('type', optional($monster)->type) == $id ? 'selected' : '' }}>
+                                        {{ $type }}
+                                    </option>
                                     @endforeach
                                 </select>
                             </div>
@@ -57,11 +53,10 @@
                                 <select class="form-select" aria-label="Default select example" name="alignment">
                                     <option selected>Select...</option>
                                     @foreach (config('variables.alignment') as $id => $alignment)
-                                        <option value="{{ $id }}" 
-                                        @if ($monster)
-                                                {{ $monster->alignment == $id ? 'selected' : '' }}
-                                        @endif
-                                            >{{ $alignment }}</option>
+                                    <option value="{{ $id }}" 
+                                        {{ old('alignment', optional($monster)->alignment) == $id ? 'selected' : '' }}>
+                                        {{ $alignment }}
+                                    </option>
                                     @endforeach
                                 </select>
                             </div>
@@ -74,10 +69,9 @@
                                     <option selected>Select...</option>
                                     @foreach (config('variables.cratureSize') as $id => $size)
                                         <option value="{{ $id }}" 
-                                        @if ($monster)
-                                                {{ $monster->size_id == $id ? 'selected' : '' }}
-                                        @endif
-                                            >{{ $size }}</option>
+                                            {{ old('size_id', optional($monster)->size_id) == $id ? 'selected' : '' }}>
+                                            {{ $size }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
@@ -89,10 +83,7 @@
                             <label for="speed" class="col-sm-3 col-form-label">Speed</label>
                             <div class="col-sm-9">
                                 <input type="number" class="form-control" id="speed" name="speed"
-                                @if ($monster)
-                                    value="{{ $monster->speed }}"
-                                @endif
-                                >
+                                value="{{ old('speed', optional($monster)->speed) }}">
                             </div>
                         </div>
 
@@ -100,10 +91,7 @@
                             <label for="ac" class="col-sm-3 col-form-label">AC</label>
                             <div class="col-sm-9">
                                 <input type="number" class="form-control" id="ac" name="ac"
-                                @if ($monster)
-                                    value="{{ $monster->ac }}"
-                                @endif
-                                >
+                                    value="{{ old('ac', optional($monster)->ac) }}">
                             </div>
                         </div>
 
@@ -111,10 +99,7 @@
                             <label for="armor" class="col-sm-3 col-form-label">Armor</label>
                             <div class="col-sm-9">
                                 <input type="text" class="form-control" id="armor" name="armor"
-                                @if ($monster)
-                                    value="{{ $monster->armor }}"
-                                @endif
-                                >
+                                    value="{{ old('armor', optional($monster)->armor) }}">
                             </div>
                         </div>
 
@@ -122,10 +107,7 @@
                             <label for="hp" class="col-sm-3 col-form-label">HP</label>
                             <div class="col-sm-9">
                                 <input type="text" class="form-control" id="hp" name="hp" placeholder="Pattern: 1d8+2"
-                                @if ($monster)
-                                    value="{{ $monster->hp }}"
-                                @endif
-                                >
+                                    value="{{ old('hp', optional($monster)->hp) }}">
                             </div>
                         </div>
 
@@ -135,60 +117,42 @@
                             <label for="str" class="col-sm-3 col-form-label">Strength</label>
                             <div class="col-sm-9">
                                 <input type="number" class="form-control" id="str" name="str" 
-                                @if ($monster)
-                                    value="{{ $monster->str }}"
-                                @endif
-                                >
+                                value="{{ old('str', optional($monster)->str) }}">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="dex" class="col-sm-3 col-form-label">Dexterity</label>
                             <div class="col-sm-9">
                                 <input type="number" class="form-control" id="dex" name="dex" 
-                                @if ($monster)
-                                    value="{{ $monster->dex }}"
-                                @endif
-                                >
+                                    value="{{ old('dex', optional($monster)->dex) }}">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="con" class="col-sm-3 col-form-label">Constitution</label>
                             <div class="col-sm-9">
                                 <input type="number" class="form-control" id="con" name="con" 
-                                @if ($monster)
-                                    value="{{ $monster->con }}"
-                                @endif
-                                >
+                                    value="{{ old('con', optional($monster)->con) }}">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="int" class="col-sm-3 col-form-label">Intelligence</label>
                             <div class="col-sm-9">
                                 <input type="number" class="form-control" id="int" name="int" 
-                                @if ($monster)
-                                    value="{{ $monster->int }}"
-                                @endif
-                                >
+                                    value="{{ old('int', optional($monster)->int) }}">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="wis" class="col-sm-3 col-form-label">Wizdom</label>
                             <div class="col-sm-9">
                                 <input type="number" class="form-control" id="wis" name="wis" 
-                                @if ($monster)
-                                    value="{{ $monster->wis }}"
-                                @endif
-                                >
+                                    value="{{ old('wis', optional($monster)->wis) }}">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="cha" class="col-sm-3 col-form-label">Charisma</label>
                             <div class="col-sm-9">
                                 <input type="number" class="form-control" id="cha" name="cha" 
-                                @if ($monster)
-                                    value="{{ $monster->cha }}"
-                                @endif
-                                >
+                                    value="{{ old('cha', optional($monster)->cha) }}">
                             </div>
                         </div>
 
@@ -198,10 +162,7 @@
                             <label for="saves" class="col-sm-3 col-form-label">Saving Throws</label>
                             <div class="col-sm-9">
                                 <input type="text" class="form-control" id="saves" name="saves"
-                                @if ($monster)
-                                    value="{{ $monster->saves }}"
-                                @endif
-                                >
+                                    value="{{ old('saves', optional($monster)->saves) }}">
                             </div>
                         </div>
 
@@ -209,10 +170,7 @@
                             <label for="skills" class="col-sm-3 col-form-label">Skills</label>
                             <div class="col-sm-9">
                                 <input type="text" class="form-control" id="skills" name="skills"
-                                @if ($monster)
-                                    value="{{ $monster->skills }}"
-                                @endif
-                                >
+                                    value="{{ old('skills', optional($monster)->skills) }}">
                             </div>
                         </div>
 
@@ -220,10 +178,7 @@
                             <label for="languages" class="col-sm-3 col-form-label">Language</label>
                             <div class="col-sm-9">
                                 <input type="text" class="form-control" id="languages" name="languages"
-                                @if ($monster)
-                                    value="{{ $monster->languages }}"
-                                @endif
-                                >
+                                    value="{{ old('languages', optional($monster)->languages) }}">
                             </div>
                         </div>
                         
@@ -231,17 +186,17 @@
                         
                         <div class="mb-3">
                             <label for="description" class="form-label">Description</label>
-                            <textarea class="form-control" id="description" rows="3" name="description">{{ $monster ? $monster->description : "" }}</textarea>
+                            <textarea class="form-control" id="description" rows="3" name="description">{{ old('description', optional($monster)->description) }}</textarea>
                         </div>
 
                         <div class="mb-3">
                             <label for="traits" class="form-label">Traits</label>
-                            <textarea class="form-control" id="traits" rows="3" name="traits">{{ $monster ? $monster->traits : "" }}</textarea>
+                            <textarea class="form-control" id="traits" rows="3" name="traits">{{ old('traits', optional($monster)->traits) }}</textarea>
                         </div>
 
                         <div class="mb-3">
                             <label for="actions" class="form-label">Actions</label>
-                            <textarea class="form-control" id="actions" rows="3" name="actions">{{ $monster ? $monster->actions : "" }}</textarea>
+                            <textarea class="form-control" id="actions" rows="3" name="actions">{{ old('actions', optional($monster)->actions) }}</textarea>
                         </div>
 
                         <hr>
@@ -252,11 +207,10 @@
                                 <select class="form-select" aria-label="Default select example" name="challenge">
                                     <option selected>Select...</option>
                                     @foreach (config('variables.challenge') as $id => $challenge)
-                                        <option value="{{ $id }}" 
-                                        @if ($monster)
-                                                {{ $monster->challenge == $id ? 'selected' : '' }}
-                                        @endif
-                                            >{{ $id }} - ({{ $challenge }} XP)</option>
+                                    <option value="{{ $id }}" 
+                                        {{ old('challenge', optional($monster)->challenge) == $id ? 'selected' : '' }}>
+                                        {{ $id }} - ({{ $challenge }} XP)
+                                    </option>
                                     @endforeach
                                 </select>
                             </div>
